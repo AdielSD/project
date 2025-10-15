fef327f87361bcac67d695122f6a48382063c6f4
AdielSD <sephorazrpm@gmail.com>
2025-10-15 05:08:03 -0500

    website plan

---
+++ scripts/siteplan.js
@@ -0,0 +1,6 @@
+
+const currentyear = new Date().getFullYear();
+document.getElementById('currentyear').textContent = currentyear;
+
+document.getElementById('lastModified').textContent =
+  "Last modification: " + document.lastModified;
