6c2e2f0ce25f9c93d8eb678569461d547d161791
AdielSD <sephorazrpm@gmail.com>
2025-10-16 18:30:21 -0500

    yes

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,15 +1,17 @@
-const menuToggle = document.getElementById('menu-toggle');
-const closeMenu = document.getElementById('close-menu');
-const navbar = document.querySelector('.navbar');
 
-menuToggle.addEventListener('click', () => {
-  navbar.classList.add('show');
-  menuToggle.classList.add('hide');
-  closeMenu.classList.add('show');
-});
+  const menuToggle = document.querySelector('.menu-toggle');
+  const closeMenu = document.querySelector('.close-menu');
+  const navbar = document.querySelector('.navbar');
 
-closeMenu.addEventListener('click', () => {
-  navbar.classList.remove('show');
-  menuToggle.classList.remove('hide');
-  closeMenu.classList.remove('show');
-});
+  menuToggle.addEventListener('click', () => {
+    navbar.classList.add('show');
+    closeMenu.classList.add('show');
+    menuToggle.classList.add('hide');
+  });
+
+  closeMenu.addEventListener('click', () => {
+    navbar.classList.remove('show');
+    closeMenu.classList.remove('show');
+    menuToggle.classList.remove('hide');
+  });
+
